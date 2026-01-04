db021611d12d9921744949e65c17583089b5bea9
Aquizu Mariano <marianoaquizu@hotmail.com>
2026-01-04 23:30:28 +0100

    Agregando responsividad y componetizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import searchIcon from './assets/search.svg';
-import locationIcon from './assets/location.svg';
-import windIcon from './assets/wind.svg';
+import { useState, useEffect } from 'react';
+import WeatherData from './Components/WeatherData.jsx';
+import Humidity from './Components/Humidity.jsx';
+import InputSearch from './Components/InputSearch.jsx';
+import Wind from './Components/Wind.jsx';
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
-  const [, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  function searchCity() {
-    const cityInput = document.getElementById('cityName');
-    const city = cityInput.value.trim();
-    
-    if (city) {
-      fetchWeatherData(city);
-    }
-  }
-
-  async function fetchWeatherData(city) {
+  const fetchWeatherData = async (city) => {
     const API_KEY = '8ca42f5f084ccade5b045c88b96cb475';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=es`;
 
@@ -30,7 +22,7 @@ function App() {
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     fetchWeatherData('london');
@@ -39,21 +31,20 @@ function App() {
   return (
     <>
       <main className='bg-[url(src/assets/background.jpeg)] h-screen w-screen bg-cover flex flex-col'>
-        <section className='w-1/2 backdrop-blur-sm drop-shadow-black drop-shadow-lg flex flex-col items-center justify-center m-auto rounded-2xl bg-black/5 p-4'>
-          <div className='w-full flex justify-center m-auto p-4'>
-            <div className='flex items-center border rounded-3xl px-2 py-1'>
-              <input className='bg-inherit py-1 px-2' required id="cityName" type="text" placeholder="Search for a city..." />
-              <button className='' onClick={searchCity}><img src={searchIcon} alt="Search Icon" /></button>
-            </div>
-          </div>
-          <div className='flex text-center flex-col items-center justify-center p-4'>
+        <section className='w-3/4 md:max-w-[700px] h-3/4 backdrop-blur-sm drop-shadow-black drop-shadow-lg justify-center m-auto rounded-2xl bg-black/5 p-4'>
+          <InputSearch onSearch={fetchWeatherData} />
+          <div className='flex flex-col mt-9 items-center text-[30px]'>
+            {loading ? (
+              <p>Cargando...</p>
+            ) : weatherData ? (
               <>
-                <div className='flex'><img src={locationIcon} alt="Location" /><h1 className='capitaliza text-[34px]'>{weatherData.name}</h1></div>
-                <h1 className='text-[30px]'>{weatherData.main.temp}°C</h1>
-                <h2 className='capitalize'>{weatherData.weather[0].description}</h2>
-                <h3>Humedad: {weatherData.main.humidity}%</h3>
-                <div className='flex'><img src={windIcon} alt="Wind Icon" /><h3>{weatherData.wind.speed} km/h</h3></div>
+                <WeatherData weatherData={weatherData} />
+                <Humidity weatherData={weatherData} />
+                <Wind weatherData={weatherData} />
               </>
+            ) : (
+              <p>No hay datos disponibles</p>
+            )}
           </div>
         </section>
       </main>
